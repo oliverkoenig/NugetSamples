54ac87d756411b109d41822cef3632215697311f
okoenig <oliver.koenig@mesalvo.de>
2024-01-03 16:04:58 +0100

    format files

--- CatalogReaderExample/CatalogReaderExample/CatalogEntities.cs
+++ CatalogReaderExample/CatalogReaderExample/CatalogEntities.cs
@@ -2,41 +2,40 @@
 using System.Collections.Generic;
 using Newtonsoft.Json;
 
-namespace CatalogReaderExample
+namespace CatalogReaderExample;
+
+public abstract class CatalogEntity
 {
-    public abstract class CatalogEntity
-    {
-        [JsonProperty("@id")]
-        public string Url { get; set; }
+  [JsonProperty("@id")]
+  public string Url { get; set; }
 
-        public DateTime CommitTimeStamp { get; set; }
-    }
+  public DateTime CommitTimeStamp { get; set; }
+}
 
-    public class CatalogIndex : CatalogEntity
-    {
-        public List<CatalogPage> Items { get; set; }
-    }
+public class CatalogIndex : CatalogEntity
+{
+  public List<CatalogPage> Items { get; set; }
+}
 
-    public class CatalogPage : CatalogEntity
-    {
-        public List<CatalogLeaf> Items { get; set; }
-    }
+public class CatalogPage : CatalogEntity
+{
+  public List<CatalogLeaf> Items { get; set; }
+}
 
-    public class CatalogLeaf : CatalogEntity
-    {
-        [JsonProperty("nuget:id")]
-        public string Id { get; set; }
+public class CatalogLeaf : CatalogEntity
+{
+  [JsonProperty("nuget:id")]
+  public string Id { get; set; }
 
-        [JsonProperty("nuget:version")]
-        public string Version { get; set; }
+  [JsonProperty("nuget:version")]
+  public string Version { get; set; }
 
-        [JsonProperty("@type")]
-        public string Type { get; set; }
-    }
+  [JsonProperty("@type")]
+  public string Type { get; set; }
+}
 
-    public class Cursor
-    {
-        [JsonProperty("value")]
-        public DateTime Value { get; set; }
-    }
+public class Cursor
+{
+  [JsonProperty("value")]
+  public DateTime Value { get; set; }
 }
